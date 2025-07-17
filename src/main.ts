fc6e5a92237c2c1451c3f412b1e2e46103835bf5
sridhar <sridhar.upstreamgs@gmail.com>
2025-07-17 15:45:32 +0530

    chore: commit latest changes

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    const loader = document.getElementById('global-loader');
+    if (loader) loader.remove();
+  })
   .catch((err) => console.error(err));
